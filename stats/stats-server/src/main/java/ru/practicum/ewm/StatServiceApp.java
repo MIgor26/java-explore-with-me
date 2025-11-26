f278bb66ad7b21c268c45ae275344166044c99fb
Igor Mikhaylov <pomivan1@yandex.ru>
2025-11-26 07:59:24 +0300

    Diplom.Stat_svc.Docker2

---
+++ stats/stats-server/src/main/java/ru/practicum/ewm/StatServiceApp.java
@@ -0,0 +1,10 @@
+package ru.practicum.ewm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class StatServiceApp {
+    public static void main(String[] args) {SpringApplication.run(StatServiceApp.class, args);}
+}
---
+++ stats/stats-server/src/main/java/ru/practicum/ewm/service/StatsService.java
@@ -0,0 +1,12 @@
+package ru.practicum.ewm.service;
+
+import ru.practicum.ewm.EndpointHitDto;
+import ru.practicum.ewm.ViewStatsDto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface StatsService {
+    void addHit(EndpointHitDto hitDto);
+    List<ViewStatsDto> getStats(LocalDateTime start, LocalDateTime end, List<String> uris, boolean unique);
+}
