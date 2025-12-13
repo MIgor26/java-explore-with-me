d61382fd49388bd082446e7c129f086627a89b65
Igor Mikhaylov <pomivan1@yandex.ru>
2025-12-13 22:20:06 +0300

    Diplom-Correct-Stat-Validate_Date_Start_End

--- stats/stats-server/src/main/java/ru/practicum/ewm/StatsController.java
+++ stats/stats-server/src/main/java/ru/practicum/ewm/StatsController.java
@@ -25,8 +25,8 @@ public class StatsController {
     }
 
     @GetMapping("/stats")
-    public List<ViewStatsDto> getStats(@RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime start,
-                                       @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime end,
+    public List<ViewStatsDto> getStats(@RequestParam(required = false) @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime start,
+                                       @RequestParam(required = false) @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime end,
                                        @RequestParam(required = false) List<String> uris,
                                        @RequestParam(defaultValue = "false") boolean unique) {
         log.info("GET/stats in StatsController. start = {}, end = {}, uris = {}, unique = {}", start, end, uris.getFirst(), unique);
